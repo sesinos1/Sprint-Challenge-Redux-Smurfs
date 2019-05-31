07de359c1f59707119511be2912a18ff38e1fcbe
sesinos tesfamariam <sesinostesfamariam@sesinoss-mbp.attlocal.net>
2019-05-31 10:28:16 -0700

    css

--- smurfs/src/components/SmurfFrom.js
+++ smurfs/src/components/SmurfFrom.js
@@ -23,35 +23,37 @@ class SmurfForm extends Component {
 
   render() {
     return (
-      <form className="Column-Layout">
-        <input
-          className="input"
-          value={this.state.name}
-          name="name"
-          type="text"
-          placeholder="Name"
-          onChange={this.handleInputChange}
-        />
-        <input
-          className="input"
-          value={this.state.age}
-          name="age"
-          type="text"
-          placeholder="Age"
-          onChange={this.handleInputChange}
-        />
-        <input
-          className="input"
-          value={this.state.height}
-          name="height"
-          type="text"
-          placeholder="Height"
-          onChange={this.handleInputChange}
-        />
-        <button onClick={() => this.handleAddSmurf()} type="button">
-          Add New Smurf
-        </button>
-      </form>
+        <div className="Column-change">
+            <form className="Column-Layout">
+                <input
+                className="input"
+                value={this.state.name}
+                name="name"
+                type="text"
+                placeholder="Name"
+                onChange={this.handleInputChange}
+                />
+                <input
+                className="input"
+                value={this.state.age}
+                name="age"
+                type="text"
+                placeholder="Age"
+                onChange={this.handleInputChange}
+                />
+                <input
+                className="input"
+                value={this.state.height}
+                name="height"
+                type="text"
+                placeholder="Height"
+                onChange={this.handleInputChange}
+                />
+                <button onClick={() => this.handleAddSmurf()} type="button">
+                Add New Smurf
+                </button>
+            </form>
+        </div>
     );
   }
 }
